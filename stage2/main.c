4d60715076a96b23a475c999e33c00e4ad17fa1f
Hector Martin <hector@marcansoft.com>
2010-09-30 06:19:05 +0200

    Clean up after lv2 (not complete yet)

--- stage2/main.c
+++ stage2/main.c
@@ -151,6 +151,106 @@ void start_net_ops(void)
 	tftp_get(tftp, (char*)eth.dhcp->boot_file_name, recv_buf, 10*1024*1024, tftp_cb, NULL);
 }
 
+void lv2_cleanup(void)
+{
+	s64 result;
+	int i,j;
+	printf("Cleaning up after Lv-2...\n");
+
+	u64 ppe_id;
+	result = lv1_get_logical_ppe_id(&ppe_id);
+
+	printf("PPE id: %ld\n", ppe_id);
+
+	u64 vas_id;
+	result = lv1_get_virtual_address_space_id_of_ppe(ppe_id , &vas_id );
+	printf("VAS id: %ld\n", vas_id);
+
+	for (i=0; i<16; i++)
+		if (lv1_destruct_virtual_address_space(i) == 0)
+			printf("Destroyed VAS %d\n", i);
+
+	int count = 0;
+
+	for (i=0; i<512; i++)
+		for (j=0; j<2; j++)
+			if (lv1_disconnect_irq_plug_ext(ppe_id, j, i) == 0)
+				count++;
+
+	printf("Disconnected %d IRQ plugs\n", count);
+
+	for (i=0; i<256; i++) {
+		if (lv1_disable_logical_spe(i, 0) == 0)
+			printf("Disabled logical spe %d\n", i);
+		if (lv1_destruct_logical_spe(i) == 0)
+			printf("Destroyed logical spe %d\n", i);
+	}
+
+	u64 size = 256*1024*1024;
+	int blocks = 0;
+	u64 total = 0;
+
+	printf("Allocating all possible lv1 memory...\n");
+
+	u64 ctr_max = 0;
+
+	while (size >= 4096) {
+		u64 newaddr;
+		u64 muid;
+		u64 cur_ctr;
+		result = lv1_allocate_memory(size, 12, 0, 0, &newaddr, &muid);
+		if (result) {
+			size >>= 1;
+			continue;
+		}
+		cur_ctr = (newaddr & 0x03fffffff000)>>12;
+		if (cur_ctr > ctr_max)
+			ctr_max = cur_ctr;
+		blocks++;
+		total += size;
+	}
+
+	printf("Allocated %ld bytes in %d blocks\n", total, blocks);
+	printf("Current allocation address counter is 0x%lx\n", ctr_max);
+
+	printf("Brute forcing Lv1 allocation regions...\n");
+
+	u64 counter;
+	u64 sbits;
+	total = 0;
+	blocks = 0;
+	u64 prev = -1;
+
+	u64 ctr_limit = 0x40000;
+
+	for (counter = 0; counter <= ctr_limit; counter++) {
+		for (sbits = 12; sbits <= 30; sbits++) {
+			u64 addr = (counter<<12) | (sbits<<42);
+			if (counter > ctr_max && (addr & 0xfffff))
+				continue;
+			u64 start_address, size, access_rights, page_size, flags;
+			result = lv1_query_logical_partition_address_region_info(addr,
+				&start_address, &size, &access_rights, &page_size, &flags);
+			if (result == 0 && start_address != prev) {
+				printf("%012lx: size %7lx rights %lx pagesize %ld (%6x) flags 0x%016lx ", start_address, size, access_rights, page_size, 1<<page_size, flags);
+				result = lv1_release_memory(start_address);
+				if (result == 0) {
+					blocks++;
+					total += size;
+					printf("CLEANED\n");
+				} else if (flags != 0) {
+					printf("IOMEM\n");
+				} else {
+					printf("ERROR\n");
+				}
+				prev = start_address;
+			}
+		}
+	}
+
+	printf("Cleaned up %ld bytes in %d blocks\n", total, blocks);
+}
+
 int main(void) {
 	debug_init();
 	printf("\n\nAsbestOS Stage 2 starting.\n");
@@ -158,6 +258,8 @@ int main(void) {
 	printf("Waiting for thread 1...\n");
 	while(!_thread1_active);
 	printf("Thread 1 is alive, all systems go.\n");
+
+	lv2_cleanup();
 
 	net_init();
 	net_loop();
