b3d3112588ce3f081d7e7f1813bcdd2d3b4312d0
Hector Martin <hector@marcansoft.com>
2011-01-01 22:05:20 +0100

    Fix broken udelay

--- stage2/time.c
+++ stage2/time.c
@@ -12,17 +12,19 @@ see file COPYING or http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 u64 gettb(void)
 {
 	u64 tb;
-	asm ("mftb %0" : "=r"(tb));
+	do {
+		asm volatile ("mftb %0" : "=r"(tb));
+	} while ((u32)tb == 0);
 	return tb;
 }
 
 void udelay(u64 usec)
 {
 	u64 now = gettb();
-	u64 later = now + (usec * (TIMEBASE_FREQ/1000)) / 1000;
-	
-	while (later < now)
-		now = gettb();
-	while (now < later)
-		now = gettb();
+	u64 later;
+	u64 ticks = usec * TICKS_PER_MS / 1000;
+
+	do {
+		later = gettb();
+	} while ((later - now) < ticks);
 }
